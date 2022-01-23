faecab70d3f9f817481515f99e007ea1bab83a35
Cole Kissane <cole.kissane@gmail.com>
2022-01-22 22:08:50 -0800

    fix: remove d3 import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import classes from "./App.module.scss";
-import * as d3 from "d3";
 import useResizeObserver from "@react-hook/resize-observer";
 import {
   Slider,
